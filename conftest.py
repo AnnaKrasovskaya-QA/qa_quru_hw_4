a125346c4a811a3e0162049d1c5b2505ef4f7faa
Anna Krasovskaya <krasovskajaanna16@gmail.com>
2025-01-04 00:37:21 +0300

    first change

--- conftest.py
+++ conftest.py
@@ -5,3 +5,4 @@ from selene import browser
 @pytest.fixture()
 def open_browser():
     browser.open('https://demoqa.com/automation-practice-form')
+
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from selene import browser
+from selenium import webdriver
+
+
+driver_options = webdriver.ChromeOptions()
+driver_options.page_load_strategy = 'eager'
+browser.config.driver_options = driver_options
 
 def test_filling_out_the_form(open_browser):
     browser.element('#firstName').type('Anna')
     browser.element('#lastName').type('Ivanova')
     browser.element('#userEmail').type('AnnaIvanova@mail.ru')
-    browser.quit()
+    browser.element('[for = gender-radio-2]').click()
+    browser.element('#userNumber').type('8978675432')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').click().element('[value="10"]').click()
+    browser.element('.react-datepicker__year-select').click().element('[value="2001"]').click()
+    browser.element('.react-datepicker__day--019').click()
+    browser.element('#subjectsInput').type('Maths').press_enter()
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('#uploadPicture')
+
+
